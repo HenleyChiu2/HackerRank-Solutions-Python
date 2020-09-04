64f19ec13fe471552ec1abf20e7d2f114852f944
HenleyChiu2 <13hc37@queensu.ca>
2020-09-04 14:17:07 -0700

    Add files via upload

---
+++ Problem Solving/CamelCase.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the camelcase function below.
+def camelcase(s):
+    if len(s) == 0:
+        return 0
+
+    count = 1
+    
+    for letter in s:
+        if letter.isupper():
+            count += 1
+
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
